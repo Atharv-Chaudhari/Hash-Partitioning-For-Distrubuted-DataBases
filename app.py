79688dbbbfec2df5209c71798cfecd1496c80b94
Atharv Chaudhari <61587515+Atharv-Chaudhari@users.noreply.github.com>
2022-01-23 21:29:58 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import mysql.connector
 import html
 import collections
 import logging
+import logging
+from threading import Thread
+from datetime import datetime
+
+data = [0 for i in range(150)]
 
 app = Flask(__name__)
 
@@ -103,14 +108,32 @@ def partition():
         names1 = "SELECT * FROM "+i[0]
         mycursor1.execute(names1)
         myresult1.append(mycursor1.fetchall())
-        names1 = "SELECT Column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME= '" + \
+        names1 = "SELECT distinct Column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME= '" + \
             i[0]+"' ORDER BY ORDINAL_POSITION"
         mycursor1.execute(names1)
         columns1.append(mycursor1.fetchall())
     return render_template('index.html', table=[], column=[], data=[], n=0, table1=tables1, column1=columns1, data1=myresult1, n1=len(tables1),f=0,cols=[],data2=[],method="")
 
+def fun(search,i):
+    print("Starting Thread",i,datetime.now().strftime("%H:%M:%S"))
+    mydb1 = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="temp1"
+    )
+    mycursor1 = mydb1.cursor()
+    mycursor1.execute(search)
+    t=mycursor1.fetchall()
+    if(len(t)!=0):
+        data[i]=t
+    print("Ending Thread",i,datetime.now().strftime("%H:%M:%S"))
+    return
+
 @app.route('/point_search', methods=['POST', 'GET'])
 def point_search():
+    global data
+    threads = []
     mydb1 = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -120,21 +143,33 @@ def point_search():
     mycursor1 = mydb1.cursor()
     mycursor1.execute("show tables")
     no_tables=len(mycursor1.fetchall())
-    names = "SELECT Column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME= 'partition_1' ORDER BY ORDINAL_POSITION"
+    names = "SELECT distinct Column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME= 'partition_1' ORDER BY ORDINAL_POSITION"
     mycursor1.execute(names)
     cols=mycursor1.fetchall()
     column_name="Id"
     id=1
-    data=[]
+    data= [0 for i in range(150)]
+    # for i in range(1,no_tables+1):
+    #     mycursor1.execute("select * from partition_"+str(i)+" where "+column_name+"='"+str(id)+"'")
+    #     t=mycursor1.fetchall()
+    #     if(len(t)!=0):
+    #         data.append(t)
     for i in range(1,no_tables+1):
-        mycursor1.execute("select * from partition_"+str(i)+" where "+column_name+"='"+str(id)+"'")
-        t=mycursor1.fetchall()
-        if(len(t)!=0):
-            data.append(t)
+        threads.append(Thread(target = fun, args = ("select * from partition_"+str(i)+" where "+column_name+"='"+str(id)+"'",i-1)))
+
+    for i in range(1,no_tables+1):
+        threads[i-1].start()
+    
+    for i in range(1,no_tables+1):
+        threads[i-1].join()
+    data=list(filter(lambda a: a != 0, data))
+    print(data)
     return render_template('index.html', table=[], column=[], data=[], n=0, table1=[], column1=[], data1=[], n1=0,f=1,cols=cols,data2=data[0],method="Result Of Point Query")
 
 @app.route('/range_search', methods=['POST', 'GET'])
 def range_search():
+    global data
+    threads = []
     mydb1 = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -144,23 +179,39 @@ def range_search():
     mycursor1 = mydb1.cursor()
     mycursor1.execute("show tables")
     no_tables=len(mycursor1.fetchall())
-    names = "SELECT Column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME= 'partition_1' ORDER BY ORDINAL_POSITION"
+    names = "SELECT distinct Column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME= 'partition_1' ORDER BY ORDINAL_POSITION"
     mycursor1.execute(names)
     cols=mycursor1.fetchall()
     column_name="Id"
-    range1=10
-    range2=40
-    data=[]
+    range1=20
+    range2=50
+    data= [0 for i in range(150)]
+    # for i in range(1,no_tables+1):
+    #     mycursor1.execute("select * from partition_"+str(i)+" where "+column_name+" BETWEEN "+str(range1)+" AND "+str(range2))
+    #     t=mycursor1.fetchall()
+    #     if(len(t)!=0):
+    #         data=data+t
+    # print(data)
     for i in range(1,no_tables+1):
-        mycursor1.execute("select * from partition_"+str(i)+" where "+column_name+" BETWEEN "+str(range1)+" AND "+str(range2))
-        t=mycursor1.fetchall()
-        if(len(t)!=0):
-            data=data+t
-    print(data)
-    return render_template('index.html', table=[], column=[], data=[], n=0, table1=[], column1=[], data1=[], n1=0,f=2,cols=cols,data2=data,method="Result Of Range Query")
+        threads.append(Thread(target = fun, args = ("select * from partition_"+str(i)+" where "+column_name+" BETWEEN "+str(range1)+" AND "+str(range2),i-1)))
+
+    for i in range(1,no_tables+1):
+        threads[i-1].start()
+    
+    for i in range(1,no_tables+1):
+        threads[i-1].join()
+    data=list(filter(lambda a: a != 0, data))
+    temp_data=[]
+    c=0
+    for i in data:
+        temp_data=temp_data+data[c]
+        c=c+1
+    return render_template('index.html', table=[], column=[], data=[], n=0, table1=[], column1=[], data1=[], n1=0,f=2,cols=cols,data2=temp_data,method="Result Of Range Query")
 
 @app.route('/all_scan_search', methods=['POST', 'GET'])
 def all_scan_search():
+    global data
+    threads = []
     mydb1 = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -170,18 +221,32 @@ def all_scan_search():
     mycursor1 = mydb1.cursor()
     mycursor1.execute("show tables")
     no_tables=len(mycursor1.fetchall())
-    names = "SELECT Column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME= 'partition_1' ORDER BY ORDINAL_POSITION"
+    names = "SELECT distinct Column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME= 'partition_1' ORDER BY ORDINAL_POSITION"
     mycursor1.execute(names)
     cols=mycursor1.fetchall()
     column_name="Species"
     value="Iris-setosa"
-    data=[]
+    data= [0 for i in range(150)]
+    # for i in range(1,no_tables+1):
+    #     mycursor1.execute("select * from partition_"+str(i)+" where "+column_name+"='"+str(value)+"'")
+    #     t=mycursor1.fetchall()
+    #     if(len(t)!=0):
+    #         data=data+t
     for i in range(1,no_tables+1):
-        mycursor1.execute("select * from partition_"+str(i)+" where "+column_name+"='"+str(value)+"'")
-        t=mycursor1.fetchall()
-        if(len(t)!=0):
-            data=data+t
-    return render_template('index.html', table=[], column=[], data=[], n=0, table1=[], column1=[], data1=[], n1=0,f=3,cols=cols,data2=data,method="Result Of All Scan Query")
+        threads.append(Thread(target = fun, args = ("select * from partition_"+str(i)+" where "+column_name+"='"+str(value)+"'",i-1)))
+
+    for i in range(1,no_tables+1):
+        threads[i-1].start()
+    
+    for i in range(1,no_tables+1):
+        threads[i-1].join()
+    data=list(filter(lambda a: a != 0, data))
+    temp_data=[]
+    c=0
+    for i in data:
+        temp_data=temp_data+data[c]
+        c=c+1
+    return render_template('index.html', table=[], column=[], data=[], n=0, table1=[], column1=[], data1=[], n1=0,f=3,cols=cols,data2=temp_data,method="Result Of All Scan Query")
 
 if __name__ == '__main__':
     app.run(debug=True)
